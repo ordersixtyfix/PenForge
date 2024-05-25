79fc9f39cde56e219a6b9a9cf89f374a01d0dba4
ordersixtyfix <75178476+ordersixtyfix@users.noreply.github.com>
2024-05-25 12:25:47 +0300

    forgot password iyilestirmeleri

--- PenForge_Frontend/src/app/forgot-password/forgot-password.js
+++ PenForge_Frontend/src/app/forgot-password/forgot-password.js
@@ -1,13 +1,41 @@
-document.getElementById('sendCodeButton').addEventListener('click', async function () {
-    const email = document.getElementById('email').value;
-
-    const response = await window.electronAPI.sendForgotPasswordRequest(`email=${email}`);
-
-    if (response.code === 200) {
-        alert("Kod başarıyla gönderildi.");
-        window.location.href = 'verify-code.html?email=' + email;
-    } else {
-        alert(response.data);
-    }
+// Yükleme ekranını sayfa yüklendiğinde gizle
+window.addEventListener('DOMContentLoaded', function() {
+    const loadingDiv = document.getElementById('loading');
+    loadingDiv.style.display = 'none';
 });
 
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    sendCode();
+});
+
+document.getElementById('sendCodeButton').addEventListener('click', async function () {
+    sendCode();
+});
+
+async function sendCode() {
+    const email = document.getElementById('email').value;
+    const loadingDiv = document.getElementById('loading');
+    const errorMessage = document.getElementById('error-message');
+
+    // Önce hata mesajını temizle
+    errorMessage.textContent = '';
+
+    // Yükleme ekranını göster
+    loadingDiv.style.display = 'flex';
+
+    try {
+        const response = await window.electronAPI.sendForgotPasswordRequest(`email=${email}`);
+
+        if (response.code === 200) {
+            window.location.href = 'verify-code.html?email=' + email;
+        } else {
+            errorMessage.textContent = response.data;
+        }
+    } catch (error) {
+        errorMessage.textContent = "Bir hata oluştu: " + error.message;
+    } finally {
+        // Yükleme ekranını gizle
+        loadingDiv.style.display = 'none';
+    }
+}
